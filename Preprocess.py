1794bd9033c33def4aa9713cbcc550d74f9f2d8c
sek788432 <48002319+sek788432@users.noreply.github.com>
2021-05-06 23:26:33 +0800

    Update Preprocess.py

--- Preprocess.py
+++ Preprocess.py
@@ -50,7 +50,7 @@ def split_sentence(text,label,sequence_length):
         while i < len(article):
             txt_sequence = []
             label_sequence =[]
-            while i < len(article) and len(txt_sequence) < sequence_length :   #and len(txt_sequence) < sequence_length
+            while i < len(article) and len(txt_sequence) < sequence_length : 
                 if article[i] == "，" or article[i]  == "。" or article[i]  == '?':
                     txt_sequence+=article[start_ind:i+1]
                     label_sequence+=label[article_ID][start_ind:i+1]
